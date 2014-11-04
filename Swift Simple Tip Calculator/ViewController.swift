c02dbc5df19257449b3d87e3447ff07047d47fe0
Kyle Tolle <kyle@nullsix.com>
2014-11-03 19:24:01 -0700

    Initial Commit

---
+++ Swift Simple Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Simple Tip Calculator
+//
+//  Created by Kyle Tolle on 11/3/14.
+//  Copyright (c) 2014 Kyle Tolle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
